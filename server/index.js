3497200fbb94bd1a28637f9ec8df54c0f5873b6f
Abhay Joshi <abhy.joshi@gmail.com>
2019-02-03 17:37:32 +0530

    tried get APIs without db queries and await or cb

--- server/index.js
+++ server/index.js
@@ -69,16 +69,22 @@ app.get('/', (req, res) => {
   res.send('Hi');
 });
 
-app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
+app.get('/values/all', (req, res) => {
+  // const values = await pgClient.query('SELECT * from values');
 
-  res.send(values.rows);
+  res.send([{
+		number: 5
+	}]);
 });
 
 app.get('/values/current', async (req, res) => {
-  redisClient.hgetall('values', (err, values) => {
-    res.send(values);
-  });
+	res.send({
+		5: 8,
+		6: 13
+	});
+  // redisClient.hgetall('values', (err, values) => {
+  //   res.send(values);
+  // });
 });
 
 app.post('/values', async (req, res) => {
